58b1ac84b25616dcf1ea13ae54b5ac1afeb91bf3
Ragnhild <ragu0002@stud.kea.dk>
2026-05-06 12:46:30 +0200

    ragnhild-base-structure-and-font

---
+++ src/app/components/typography/Typography.jsx
@@ -0,0 +1,41 @@
+import "./typography.css";
+
+export const Hero = ({ text, color, font = "font-helvetica" }) => (
+  <h1 style={{ fontSize: "var(--step-5)" }} className={`uppercase ${color} ${font}`}>
+    {text}
+  </h1>
+);
+export const XlargeText = ({ text, color, font = "font-helvetica" }) => (
+  <h2 style={{ fontSize: "var(--step-4)" }} className={`uppercase  ${color} ${font}`}>
+    {text}
+  </h2>
+);
+export const LargeText = ({ text, color, font = "font-helvetica" }) => (
+  <h3 style={{ fontSize: "var(--step-3)" }} className={`uppercase ${color} ${font}`}>
+    {text}
+  </h3>
+);
+export const RegularText = ({ text, color, font = "font-helvetica" }) => (
+  <h4 style={{ fontSize: "var(--step-3)" }} className={`uppercase ${color} ${font}`}>
+    {text}
+  </h4>
+);
+export const Paragraph = ({ text, color, font = "font-helvetica" }) => (
+  <p style={{ fontSize: "var(--step-2)" }} className={`  ${color} ${font}`}>
+    {text}
+  </p>
+);
+export const SmallParagraph = ({ text, color, font = "font-helvetica" }) => (
+  <p style={{ fontSize: "var(--step--1)" }} className={` ${color} ${font}`}>
+    {text}
+  </p>
+);
+export const NavigationText = ({ text, color, font = "font-helvetica" }) => (
+  <li style={{ fontSize: "var(--step-1)" }} className={`uppercase ${color} ${font}`}>
+    {text}
+  </li>
+);
+
+const Typography = ({ children }) => <>{children}</>;
+
+export default Typography;
