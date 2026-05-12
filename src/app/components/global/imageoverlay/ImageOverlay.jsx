7cdc6a48f43a54ccd4c9ce6050e21f579bdb24d5
Ragnhild <ragu0002@stud.kea.dk>
2026-05-12 18:23:49 +0200

    ragnhild-workshop-info

--- src/app/components/global/imageoverlay/ImageOverlay.jsx
+++ src/app/components/global/imageoverlay/ImageOverlay.jsx
@@ -1,23 +1,12 @@
 import Image from "next/image";
 import { Hero } from "../../typography";
+import "./imageoverlay.css";
 
-const ImageOverlay = ({ variant = "two", width, height, image = "bilde_1.jpg", text }) => {
+const ImageOverlay = ({ variant = "one", width, height, image = "bilde_1.jpg", text }) => {
   const variants = {
     one: {
-      text: {
-        rowStart: "row-start-1",
-        rowSpan: "row-span-2",
-        colStart: "col-start-4",
-        colSpan: "col-span-17",
-      },
-      image: {
-        rowStart: "row-start-2",
-        rowSpan: "row-span-full",
-        colStart: "col-start-5",
-        colSpan: "col-span-16",
-        imgwidth: "100vw",
-        imgHeigth: "20px",
-      },
+      text: "text_variant_1",
+      image: "image_variant_1",
     },
     two: {
       text: {
@@ -35,16 +24,20 @@ const ImageOverlay = ({ variant = "two", width, height, image = "bilde_1.jpg", t
         imgHeigth: "h-120",
       },
     },
+    three: {
+      text: "text_variant_3",
+      image: "image_variant_3",
+    },
   };
 
   const currentVariant = variants[variant] || variants.one;
 
   return (
-    <div className={`grid grid-cols-20 grid-rows-20   ${currentVariant.image.imgwidth} ${currentVariant.image.imgHeigth}`}>
-      <div className={`${currentVariant.text.rowStart} ${currentVariant.text.rowSpan} ${currentVariant.text.colStart} ${currentVariant.text.colSpan} z-10 text-center`}>
+    <div className={`my-grid ${currentVariant.image.imgwidth} ${currentVariant.image.imgHeigth}`}>
+      <div className={`${currentVariant.text} ${currentVariant.text.rowStart} ${currentVariant.text.rowSpan} ${currentVariant.text.colStart} ${currentVariant.text.colSpan} z-10 text-center`}>
         <Hero text={text} color="accent" font="font-serif" />
       </div>
-      <div className={`rounded-4xl overflow-hidden ${currentVariant.image.rowStart} ${currentVariant.image.rowSpan} ${currentVariant.image.colStart} ${currentVariant.image.colSpan} `}>
+      <div className={`rounded-4xl overflow-hidden ${currentVariant.image} ${currentVariant.image.rowStart} ${currentVariant.image.rowSpan} ${currentVariant.image.colStart} ${currentVariant.image.colSpan} `}>
         <Image src={`/assets/images/${image}`} width={width} height={height} alt="terpsiimage" className="w-full h-full object-cover" />
       </div>
     </div>
