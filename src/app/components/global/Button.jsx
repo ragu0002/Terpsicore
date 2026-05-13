17f1e3bea05fe3c938c44a23d60ce5923c33ed68
Ragnhild <ragu0002@stud.kea.dk>
2026-05-13 17:59:53 +0200

    ragnhild-create-om-oss

--- src/app/components/global/Button.jsx
+++ src/app/components/global/Button.jsx
@@ -2,7 +2,7 @@ import { RegularText, SmallParagraph } from "../typography";
 
 const Button = ({ variant, text }) => {
   return (
-    <button className={`border rounded-4xl px-5 py-2 cursor-pointer hover:rounded-none ${variant === "dark" ? "" : "bg-foreground text-background"}`}>
+    <button className={`border rounded-4xl px-5 py-2 cursor-pointer hover:rounded-none ${variant === "dark" ? "bg-foreground text-background" : ""}`}>
       <SmallParagraph text={text} color="uppercase" />
     </button>
   );
