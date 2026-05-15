dd9d3191588a99df4c3d8f849dcc7065b578d56f
Ragnhild <ragu0002@stud.kea.dk>
2026-05-15 13:59:46 +0200

    ragnhild-make-forestilling-dynamic

--- src/app/(routes)/forestilling/page.js
+++ src/app/(routes)/forestilling/page.js
@@ -1,24 +1,21 @@
-import Galleri from "@/app/components/forestilling/Galleri";
-import Plakat from "@/app/components/forestilling/Plakat";
-import ImageOverlay from "@/app/components/global/imageoverlay/ImageOverlay";
-import { SmallParagraph } from "@/app/components/typography";
-export default function Foretilling() {
+import ForestillingClient from "@/app/components/forestilling/ForestillingClient";
+
+import { getStoryblokApi } from "@/lib/storyblok";
+export default async function Foretilling() {
+  const { data } = await fetchData();
   return (
     <>
       <main className="grid col-(--full-col) grid-cols-subgrid mt-30">
-        <div className="col-(--content-col)">
-          <ImageOverlay width="2000" height="4000" text="forestilling" image="bilde_2.jpg" variant="two" containerH="h-120" containerW="w-full" />
-          <div className="md:text-center md:grid grid-cols-5 *:col-start-2 *:col-span-3 py-5">
-            <SmallParagraph text="Årets høydepunkt er vår egenproduserte forestilling på USF Verftet som finner sted rundt mars/ april hvert år. Forestillingsprosessen starter tidlig i høstsemesteret, med innstudering konsentrert på vårsemesteret, og resulterer i en oppsetning som viser kompaniets egenkoreograferte tolkning av årets tematikk." />
-          </div>
-          <div className="flex justify-between gap-5">
-            <Plakat text="når verket våkner" year="2025" image="plakat_3.png" />
-            <Plakat text="får du ikke sove" year="2024" image="plakat_2.png" />
-            <Plakat text="veni vidi hva nå?" year="2023" image="plakat_1.png" />
-          </div>
-          <Galleri />
-        </div>
+        <ForestillingClient stories={data.stories} />
       </main>
     </>
   );
+}
+export async function fetchData() {
+  const storyblokApi = getStoryblokApi();
+
+  return await storyblokApi.get("cdn/stories", {
+    version: "draft",
+    starts_with: "forestilling/",
+  });
 }
--- src/app/components/forestilling/Plakat.jsx
+++ src/app/components/forestilling/Plakat.jsx
@@ -3,15 +3,15 @@ import { Paragraph, RegularText } from "../typography";
 
 const Plakat = ({ image = "bilde_1.jpg", text, year }) => {
   return (
-    <div className="grid text-center w-full h-auto hover:scale-97 cursor-pointer mt-15">
+    <>
       <div className="rounded-4xl overflow-hidden">
-        <Image src={`/assets/images/${image}`} width={800} height={800} alt="plakat" className="w-full h-full object-cover" />
+        <Image src={image} width={800} height={800} alt="plakat" className="w-full h-full object-cover" />
       </div>
       <div className="py-5">
         <RegularText text={text} />
         <Paragraph text={year} />
       </div>
-    </div>
+    </>
   );
 };
 
