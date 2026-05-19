10e9b996062b6f63033380b472813ce11f62dfb2
Ragnhild <ragu0002@stud.kea.dk>
2026-05-19 14:25:48 +0200

    ragnhild-add-images

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,29 @@
-import Image from "next/image";
-import { Hero, LargeText, NavigationText, Paragraph, RegularText, SmallParagraph, XlargeText } from "./components/typography";
-import Button from "./components/global/Button";
-import ImageOverlay from "./components/global/imageoverlay/ImageOverlay";
-import Underline from "./components/global/Underline";
+import { getStoryblokApi } from "@/lib/storyblok";
 import HomeHero from "./components/home/HomeHero";
 import OmOss from "./components/home/OmOss";
 import Oppdrag from "./components/home/Oppdrag";
 import WorkshopInfo from "./components/home/WorkshopInfo";
 import ForestillingGalleri from "./components/home/ForestillingGalleri";
-export default function Home() {
+export default async function Home() {
+  const { data } = await fetchData();
+  const omOssStory = data.stories[0];
   return (
     <>
       <main className="grid col-(--full-col) grid-cols-subgrid">
         <HomeHero />
-        <OmOss />
+        <OmOss stories={omOssStory} />
         <Oppdrag />
         <WorkshopInfo />
         <ForestillingGalleri />
       </main>
     </>
   );
+}
+export async function fetchData() {
+  const storyblokApi = getStoryblokApi();
+
+  return await storyblokApi.get("cdn/stories", {
+    version: "draft",
+    starts_with: "om-oss/",
+  });
 }
